bed515add9a48d4335c40d8206607a2b6e4d8eab
Danpa_cho <danpa725@cau.ac.kr>
2022-08-17 15:40:29 +0900

    Update: code css .

--- src/pages/[category]/[pageNumber]/index.tsx
+++ src/pages/[category]/[pageNumber]/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import { GetStaticPaths, GetStaticProps } from "next"
 
 import { ParsedUrlQuery } from "querystring"
@@ -8,16 +7,13 @@ import { PostMetaType } from "@typing/post/meta"
 import { CategoryInfoType } from "@typing/category/info"
 
 import {
-    getPageNumberOfCategory,
     getSpecificPostMeta,
-    getAllCategoryPaginationPath,
     getTagOfSpecificCategoryPage,
-} from "@utils/function/blog-contents-loader/contents/getCategoryPost"
-import { getSpecificCategoryInfo } from "@utils/function/blog-contents-loader/contents/getCategory"
+    getPageNumberOfCategory,
+    getAllCategoryPaginationPath,
+} from "@core/loader/post"
 
-import { NextIcon, PrevIcon } from "@components/UI/Atoms/Icons"
-import { Button } from "@components/UI/Atoms/Button"
-import { CategoryCommonLayout } from "@components/Blog/Category"
+import { getSpecificCategoryInfo } from "@core/loader/category"
 
 import { config } from "blog.config"
 
@@ -76,52 +72,7 @@ interface CategoryPostPerPageProps extends CategoryInfoType {
 }
 
 function CategoryPostPerPage(props: CategoryPostPerPageProps) {
-    const pageNumber = Number(props.pageNumber)
-
-    const { isLast, categoryUrl, category } = props
-    const isFirst = pageNumber === 1
-
-    return (
-        <CategoryCommonLayout
-            {...props}
-            pageNumber={pageNumber}
-            prevPageComponent={
-                <Link
-                    passHref
-                    href={
-                        isFirst
-                            ? categoryUrl
-                            : `${categoryUrl}/${pageNumber - 1}`
-                    }
-                >
-                    <Button
-                        ariaLabel={`to the previous page: ${pageNumber - 1}`}
-                    >
-                        <PrevIcon />
-                        {isFirst && <>{category}</>}
-                        {!isFirst && `Page ${pageNumber - 1}`}
-                    </Button>
-                </Link>
-            }
-            nextPageComponent={
-                <Link
-                    href={
-                        isLast
-                            ? categoryUrl
-                            : `${categoryUrl}/${pageNumber + 1}`
-                    }
-                    passHref
-                >
-                    <Button ariaLabel={`to the next page: ${pageNumber + 1}`}>
-                        {isLast && isFirst && `Oops It's Empty💨`}
-                        {isLast && !isFirst && "Thank you! Last page 🎉"}
-                        {!isLast && `Page ${pageNumber + 1}`}
-                        <NextIcon />
-                    </Button>
-                </Link>
-            }
-        />
-    )
+    return <></>
 }
 CategoryPostPerPage.displayName = "Category" as PageType
 
