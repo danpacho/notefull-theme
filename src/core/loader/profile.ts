b76a928315d1e33bf395d57d87b3152cba011c06
Danpa_cho <danpa725@cau.ac.kr>
2022-08-18 00:06:51 +0900

    Refactor: core profile api .

--- src/core/loader/profile.ts
+++ src/core/loader/profile.ts
@@ -1,39 +1,45 @@
-import { BLOG_PROFILE_CONTENTS_LOCATION } from "@constants/blog.contents.directory"
+import { BLOG_PROFILE_DIR } from "@constants/blog.dir"
 
 import { readFile } from "fs/promises"
 
 import { BlogErrorAdditionalInfo, BlogFileExtractionError } from "@core/error"
 
-import { memo } from "@core/loader/util"
+import { definePlugins } from "@core/loader/util"
 import { bundlePost } from "@core/loader/post"
 
-import { config } from "blog.config"
+import { remarkAutomaticImageSize } from "@lib/unified/remark"
+import remarkGfm from "remark-gfm"
+import rehypePrism from "rehype-prism-plus"
 
-const getProfileSource = memo(config.useMemo, async () => {
+const getProfileSource = async () => {
     try {
-        const profileContent = await readFile(
-            BLOG_PROFILE_CONTENTS_LOCATION,
-            "utf-8"
-        )
+        const profileContent = await readFile(BLOG_PROFILE_DIR, "utf-8")
         if (!profileContent)
             throw new BlogFileExtractionError({
                 errorNameDescription: "post file extraction error occured",
                 readingFileFormat: ".mdx",
-                readingFileLocation: BLOG_PROFILE_CONTENTS_LOCATION,
+                readingFileLocation: BLOG_PROFILE_DIR,
                 readingFileName: "profile.mdx",
             })
 
         return await (
-            await bundlePost({ postSource: profileContent })
+            await bundlePost({
+                postSource: profileContent,
+                customPlugin: definePlugins({
+                    // plugins using on profile page
+                    rehypePlugins: [rehypePrism],
+                    remarkPlugins: [remarkAutomaticImageSize, remarkGfm],
+                }),
+            })
         ).bundledResult.code
     } catch (err) {
         throw new BlogErrorAdditionalInfo({
             passedError: err,
             errorNameDescription: "profile file reading error",
             message: "",
-            customeErrorMessage: `your profile file at:\n\n   ${BLOG_PROFILE_CONTENTS_LOCATION}`,
+            customeErrorMessage: `your profile file at:\n\n   ${BLOG_PROFILE_DIR}`,
         })
     }
-})
+}
 
 export { getProfileSource }
