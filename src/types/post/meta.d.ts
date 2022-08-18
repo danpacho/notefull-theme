ae8b7e63b8d5ccaa72fbc91a49debeccf8087c3a
Danpa_cho <danpa725@cau.ac.kr>
2022-08-18 11:58:07 +0900

    Update: add optional bannerUrl for post banner and add tsdoc for dev user .

--- src/types/post/meta.d.ts
+++ src/types/post/meta.d.ts
@@ -1,16 +1,51 @@
 /**
- * meta of pure mdx string
+ * optional meta property
+ * @property `series`: `{ title }-{ Number_string }` if there is only one series, series will not displayed
+ * @property `bannerUrl`: nextjs static image url at [`public`](https://nextjs.org/docs/basic-features/static-file-serving}) folder
+ * @property `reference`: `{ ref1 }, { ref2 }, ...`
+ * @property `potpone`: `{ Boolean_string }` if `true`, post will not published
+ 
+ * @example
+ * ---
+ * series: seriesTitle-1
+ * bannerUrl: my-category/first-post/banner.png
+ * refrence: https://ref1.com, https://ref2.com, https://ref3.com 
+ * postpone: true
+ * ---
+*/
+interface OptionalMDXMetaType {
+    series?: string
+    bannerUrl?: string
+    reference?: string
+    postpone?: string
+}
+/**
+ * - total meta property
+ * - extract with `gray-matter` package
+ * - check optional meta property at {@link OptionalMDXMetaType}
+
+ * @property `series`: `{ title }-{ order }`
+ * @property `bannerUrl`: nextjs static image url at `public` folder
+ * @property `reference`: `{ ref1 }, { ref2 }, ...`
+ * @property `potpone`: if `true`, post will not published
+ * 
+ * @example
+ * ---
+ * title: this is title
+ * preview: this is preview
+ * author: this is author name
+ * update: 2022/08/15
+ * color: "#222660"
+ * tags: tag1, tag2, tag3
+ * ---
  */
-export interface MDXMetaType {
+export interface MDXMetaType extends OptionalMDXMetaType {
     title: string
     preview: string
     author: string
     update: string
     color: string
     tags: string
-    postpone?: string
-    reference?: string
-    series?: string
 }
 
 export interface SeriesMetaType {
@@ -18,7 +53,7 @@ export interface SeriesMetaType {
     order: number
 }
 /**
- * total post meta property with {@link MDXMetaType} and {@link SeriesMetaType}
+ * post meta property with {@link MDXMetaType} and {@link SeriesMetaType}
  */
 export interface MetaType {
     title: string
@@ -34,4 +69,5 @@ export interface MetaType {
     postpone: boolean
     reference: string[] | null
     series: SeriesMetaType | null
+    bannerUrl: string | null
 }
