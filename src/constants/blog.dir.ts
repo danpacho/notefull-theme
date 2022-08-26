31548f1e29286b9260441a6e75bf7359c9cc03cf
Danpa_cho <danpa725@cau.ac.kr>
2022-08-26 14:54:08 +0900

    Update: profile content location .

--- src/constants/blog.dir.ts
+++ src/constants/blog.dir.ts
@@ -3,6 +3,6 @@ import { config } from "blog.config"
 const BLOG_CONTENTS_DIR =
     `${config.blogContentsDirectoryName}/contents` as const
 const BLOG_PROFILE_DIR =
-    `${config.blogContentsDirectoryName}/profile/description.mdx` as const
+    `${config.blogContentsDirectoryName}/profile.mdx` as const
 
 export { BLOG_CONTENTS_DIR, BLOG_PROFILE_DIR }
