0b19fd4238f916918b2d0f70644d3a3296d032af
Danpa_cho <danpa725@cau.ac.kr>
2022-08-28 22:44:47 +0900

    :wrench: Update: tailwind util config and adapt base style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const plugin = require("tailwindcss/plugin")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{ts,tsx}"],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(({ addUtilities }) => {
+            addUtilities({
+                // 🚀 VSCODE_AUTO_COMPLETE 🚀
+                ".description_text": {},
+                ".heading_text": {},
+            })
+        }),
+    ],
 }
