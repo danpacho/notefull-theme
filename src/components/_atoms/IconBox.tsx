b61bfd1649dc2ae1e9fe47abfe3ba7f2ccfdbaac
Danpa_cho <danpa725@cau.ac.kr>
2022-08-29 22:12:34 +0900

    :lipstick: Update: add dark-mode `hex` opacity option at `icon-box`

--- src/components/_atoms/IconBox.tsx
+++ src/components/_atoms/IconBox.tsx
@@ -1,13 +1,32 @@
+import { useTheme } from "next-themes"
+
+import { ThemeMode } from "@typing/theme"
+import { useEffect, useState } from "react"
+
+const HEX_OPACITY = {
+    dark: "BF", // 75%
+    light: "80", // 50%
+}
+
 interface IconBoxProps {
     hex: string
     children: React.ReactNode
 }
-const IconBox = ({ hex, children }: IconBoxProps) => {
-    const opacityColor = `${hex}7d`
+function IconBox({ hex, children }: IconBoxProps) {
+    const theme = useTheme().theme as ThemeMode
+    const [color, setColor] = useState("")
+    useEffect(() => {
+        const opacityColor =
+            theme === "light"
+                ? `${hex}${HEX_OPACITY.light}`
+                : `${hex}${HEX_OPACITY.dark}`
+        setColor(opacityColor)
+    }, [theme, hex])
+
     return (
         <div
             className={`flex items-center justify-center w-6 h-6 rounded-sm text-xs font-bold`}
-            style={{ backgroundColor: opacityColor }}
+            style={{ backgroundColor: color }}
         >
             {children}
         </div>
