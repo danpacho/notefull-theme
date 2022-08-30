88d384da97451bb61ba9c43cb2d70391e2843f61
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 23:03:48 +0900

    :lipstick: Update: `_atoms` export

--- src/components/_atoms/index.ts
+++ src/components/_atoms/index.ts
@@ -1,4 +1,5 @@
-export { default as IconBox } from "./IconBox"
 export { default as ColorTitle } from "./ColorTitle"
-export { default as RowBetween } from "./RowBetween"
+export { default as IconBox } from "./IconBox"
 export { default as Grid } from "./Grid"
+export { default as RowBetween } from "./RowBetween"
+export * from "./Text"
