137f912b5b62f263dc9b7a33668f4e6dc2ecac27
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 14:57:21 +0900

    :lipstick: Update: common layout at `_app`

--- src/components/_layout/index.tsx
+++ src/components/_layout/index.tsx
@@ -4,7 +4,9 @@ import NoteBackground from "./NoteBackground"
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="flex flex-col items-center justify-center p-8 mx-auto layout">
-            {children}
+            <div className="flex flex-col items-start justify-center w-full h-full gap-4">
+                {children}
+            </div>
             <Navigation />
             <NoteBackground rectSize={150} />
         </main>
