3a183739cdcc2947c9fa1967ea9472f6b4172a26
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 14:59:27 +0900

    :recycle: Refactor: main page with added common component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import { getLatestPostMeta } from "@core/loader/post"
 
 import { config } from "blog.config"
 
-import { PostLink, CategoryLink, Banner } from "@components/_common"
+import { Banner, PostLinkLayer, CategoryLinkLayer } from "@components/_common"
 import { ColorTitle } from "@components/_atoms"
 
 export const getStaticProps: GetStaticProps<MainPageProps> = async () => {
@@ -31,7 +31,7 @@ interface MainPageProps {
 }
 function MainPage({ latestPost, mainCategory }: MainPageProps) {
     return (
-        <div className="flex flex-col items-start justify-center gap-4">
+        <>
             <NextSeo
                 title={config.siteName}
                 canonical={config.url}
@@ -48,32 +48,15 @@ function MainPage({ latestPost, mainCategory }: MainPageProps) {
                 title="Category"
                 size="text-2xl"
             />
-
-            <div className="grid w-full grid-cols-2 grid-rows-2 gap-4 md:grid-cols-3 ">
-                {mainCategory.map((category) => (
-                    <CategoryLink {...category} key={category.category} />
-                ))}
-                <CategoryLink
-                    category="Expore More"
-                    categoryUrl="/category"
-                    color="#ded235"
-                    description="click to explore more category"
-                    emoji="🍻"
-                />
-            </div>
+            <CategoryLinkLayer categoryInfoArray={mainCategory} />
 
             <ColorTitle
                 hex={config.themeColor}
                 title="Latest"
                 size="text-2xl"
             />
-
-            <div className="grid w-full grid-cols-1 grid-rows-2 gap-4 pb-8 md:grid-cols-2">
-                {latestPost.map((post) => (
-                    <PostLink {...post} key={post.title} />
-                ))}
-            </div>
-        </div>
+            <PostLinkLayer postMetaArray={latestPost} />
+        </>
     )
 }
 MainPage.displayName = "Home" as PageType
