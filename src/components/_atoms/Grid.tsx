56144e59c395852d3101c1c18d879fa368b4e4ed
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 14:55:42 +0900

    :lipstick: Add: grid atom util component

---
+++ src/components/_atoms/Grid.tsx
@@ -0,0 +1,31 @@
+import { TailwindGridRowType, TailwindGridColType } from "@typing/tailwind"
+
+interface GirdProps {
+    children: React.ReactNode
+    col: TailwindGridColType
+    row: TailwindGridRowType
+    mdCol?: `md:${TailwindGridColType}`
+    mdRow?: `md:${TailwindGridRowType}`
+    gap: `gap-${"0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"}`
+    styleClass?: string
+}
+
+function Grid({
+    children,
+    col,
+    row,
+    gap,
+    mdCol,
+    mdRow,
+    styleClass,
+}: GirdProps) {
+    return (
+        <div
+            className={`grid w-full ${row} ${col} ${gap} ${mdRow} ${mdCol} ${styleClass}`}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Grid
