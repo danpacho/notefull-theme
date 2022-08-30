9f76dc05bacb9723f88d3e769d0355ad8bc9e075
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 22:59:55 +0900

    :recycle: Refactor: replace tailwind @component layer utill class to constant
    `tw.[some-class]`

---
+++ src/styles/tailwind.util.ts
@@ -0,0 +1,7 @@
+const tw = {
+    layout: "w-full min-h-screen h-max md:w-3/4 lg:w-4/6",
+    boxBorder:
+        "border dark:border-gray-500 hover:border-black dark:hover:border-gray-100",
+} as const
+
+export default tw
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
-const plugin = require("tailwindcss/plugin")
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{ts,tsx}"],
     theme: {
         extend: {},
     },
-    plugins: [
-        plugin(({ addUtilities }) => {
-            addUtilities({
-                // 🚀 VSCODE_AUTO_COMPLETE 🚀
-                ".description_text": {},
-                ".heading_text": {},
-                ".layout": {},
-            })
-        }),
-    ],
+    plugins: [],
     darkMode: "class",
 }
