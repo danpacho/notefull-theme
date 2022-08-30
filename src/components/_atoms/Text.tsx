aeb2ff30a1ab4ee6ba96d3b647cac2225ab731c5
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 23:03:01 +0900

    :lipstick: Add: text util atom component
    `title`, `description`

---
+++ src/components/_atoms/Text.tsx
@@ -0,0 +1,44 @@
+import { TailwindFontSizeType } from "@typing/tailwind"
+
+interface TextSizeProps {
+    children: React.ReactNode
+    size?: TailwindFontSizeType
+    mdSize?: `md:${TailwindFontSizeType}`
+    styleClass?: string
+}
+
+function Title({
+    children,
+    size = "text-base",
+    mdSize = "md:text-lg",
+    styleClass,
+}: TextSizeProps) {
+    return (
+        <h1
+            className={`font-bold capitalize truncate ${size} ${mdSize} ${styleClass}`}
+        >
+            {children}
+        </h1>
+    )
+}
+
+function Description({
+    children,
+    size = "text-sm",
+    mdSize = "md:text-sm",
+    styleClass,
+}: TextSizeProps) {
+    return (
+        <div
+            className={`${size} ${mdSize} w-full overflow-hidden text-ellipsis font-normal text-gray-500 dark:text-gray-300 ${styleClass}`}
+        >
+            {children}
+        </div>
+    )
+}
+
+function Tag() {
+    return <></>
+}
+
+export { Title, Description }
