18c6b41137ecb063820972bf6dc27a4daa3a1eea
Danpa_cho <danpa725@cau.ac.kr>
2022-08-30 23:02:19 +0900

    :lipstick: Update: grid component row prop to optional

--- src/components/_atoms/Grid.tsx
+++ src/components/_atoms/Grid.tsx
@@ -3,7 +3,7 @@ import { TailwindGridRowType, TailwindGridColType } from "@typing/tailwind"
 interface GirdProps {
     children: React.ReactNode
     col: TailwindGridColType
-    row: TailwindGridRowType
+    row?: TailwindGridRowType
     mdCol?: `md:${TailwindGridColType}`
     mdRow?: `md:${TailwindGridRowType}`
     gap: `gap-${"0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"}`
