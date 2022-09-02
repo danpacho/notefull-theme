306be19735313ea2881975f6442c4499023fa359
Danpa_cho <danpa725@cau.ac.kr>
2022-09-02 13:23:12 +0900

    :recycle: Refactor: divide `navigation` component style-class to constant object

--- src/components/_layout/Navigation.tsx
+++ src/components/_layout/Navigation.tsx
@@ -14,8 +14,19 @@ const useMounted = () => {
     return { isMounted }
 }
 
-const NavigationStyle =
-    "py-1 mx-2 text-sm font-normal transition-all border-b border-b-transparent hover:border-b-black md:px-2 md:border-l md:hover:border-l-black md:hover:border-b-transparent hover:text-black dark:hover:text-gray-100"
+const NavStyle = {
+    layout: "fixed bottom-[-0.1px] flex flex-row items-start justify-center gap-2 md:gap-0 md:top-0 md:right-0 md:flex-col lg:right-[5%]",
+    box: "w-full py-2 md:w-fit",
+    bg: "bg-white dark:bg-neutral-900 md:dark:bg-transparent md:bg-transparent",
+    text: "dark:text-gray-400 dark:hover:text-gray-500 text-gray-400 hover:text-black",
+    border: "border-t border-t-gray-300 md:border-t-0",
+} as const
+
+const NavBtnStyle = {
+    box: "py-1 mx-2 md:px-2 transition",
+    border: "border-b border-b-transparent hover:border-b-black dark:hover:border-b-gray-100 md:border-l md:border-b-0 md:hover:border-l-black",
+    text: "text-sm font-normal hover:text-black dark:hover:text-gray-100",
+} as const
 
 function Navigation() {
     const { isMounted } = useMounted()
@@ -23,15 +34,22 @@ function Navigation() {
     if (!isMounted) return <></>
 
     return (
-        <nav className="fixed bottom-[-0.1px] w-full flex flex-row items-start justify-center gap-2 py-2 bg-white dark:bg-neutral-900 md:dark:bg-transparent border-t -z-0 border-t-gray-300 md:w-fit md:bg-transparent md:border-t-0 dark:text-gray-400 dark:hover:text-gray-500 text-gray-400 hover:text-black md:fixed md:gap-0 md:top-0 lg:right-[5%] md:right-0 md:flex-col">
+        <nav
+            className={`${NavStyle.layout} ${NavStyle.box} ${NavStyle.bg} ${NavStyle.border} ${NavStyle.text} z-0`}
+        >
             {config.navigationMenu.map(({ name, path }) => {
                 return (
-                    <div key={name} className={NavigationStyle}>
+                    <div
+                        key={name}
+                        className={`${NavBtnStyle.box} ${NavBtnStyle.border} ${NavBtnStyle.text}`}
+                    >
                         <Link href={path}>{name}</Link>
                     </div>
                 )
             })}
-            <ThemeBtn styleClass={NavigationStyle} />
+            <ThemeBtn
+                styleClass={`${NavBtnStyle.box} ${NavBtnStyle.border} ${NavBtnStyle.text}`}
+            />
         </nav>
     )
 }
