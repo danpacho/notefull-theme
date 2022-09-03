8e991f2f521046877575f4bdf8298710c83983db
Danpa_cho <danpa725@cau.ac.kr>
2022-09-03 16:15:25 +0900

    :recycle: Refactor: `theme-btn` with added common component and replace `use-window-theme` hook with `next-themes`'s built in `system-theme`

--- src/components/ThemeBtn/index.tsx
+++ src/components/ThemeBtn/index.tsx
@@ -1,30 +1,25 @@
-import { useEffect } from "react"
+import { ThemeMode } from "@typing/theme"
 import { useTheme } from "next-themes"
 
-import useWindowTheme from "./useWindowTheme"
-import { ThemeMode } from "@typing/theme"
+import { RenderAfterMounted } from "@components/_common"
 
 const ThemeBtn = ({ styleClass }: { styleClass: string }) => {
     const { theme, setTheme } = useTheme()
 
-    const windowTheme = useWindowTheme()
-
-    useEffect(() => {
-        setTheme(windowTheme)
-    }, [windowTheme, setTheme])
-
     return (
-        <button
-            onClick={() =>
-                (theme as ThemeMode) === "light"
-                    ? setTheme("dark")
-                    : setTheme("light")
-            }
-            className={styleClass}
-        >
-            {theme === "dark" && "Light"}
-            {theme === "light" && "Dark"}
-        </button>
+        <RenderAfterMounted>
+            <button
+                onClick={() =>
+                    (theme as ThemeMode) === "light"
+                        ? setTheme("dark")
+                        : setTheme("light")
+                }
+                className={styleClass}
+            >
+                {theme === "dark" && "Light"}
+                {theme === "light" && "Dark"}
+            </button>
+        </RenderAfterMounted>
     )
 }
 
