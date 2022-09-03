a6772b6ab8d8420887a623b4e1f8cbfe84f9eefe
Danpa_cho <danpa725@cau.ac.kr>
2022-09-03 16:06:50 +0900

    :lipstick: Update: tailwind util class `layout` and rename `box-border` to `border`

--- src/styles/tailwind.util.ts
+++ src/styles/tailwind.util.ts
@@ -1,7 +1,6 @@
 const tw = {
-    layout: "w-full min-h-screen h-max md:w-3/4 lg:w-4/6",
-    boxBorder:
-        "border dark:border-gray-500 hover:border-black dark:hover:border-gray-100",
+    layout: "w-full min-h-screen h-max md:w-3/4 lg:w-4/6 xl:w-3/5",
+    border: "border dark:border-gray-500 hover:border-black dark:hover:border-gray-100",
 } as const
 
 export default tw
