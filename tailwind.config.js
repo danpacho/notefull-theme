289e9afefd427d094a9694916b9e8a1500ec6d86
Danpa_cho <danpa725@cau.ac.kr>
2022-09-06 14:51:21 +0900

    :wrench: Update: `tailwind.config` to add `list-style-type`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            listStyleType: {
+                none: "none",
+                ["circle-filled"]: "disc",
+                circle: "circle",
+                square: "square",
+                decimal: "decimal",
+                roman: "upper-roman",
+                ["lower-alpha"]: "lower-alpha",
+                ["upper-alpha"]: "upper-alpha",
+            },
+        },
     },
     plugins: [],
     darkMode: "class",
