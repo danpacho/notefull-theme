89799bc89a5665f06f3726b130f716e7a699812f
Danpa_cho <danpa725@cau.ac.kr>
2022-09-06 16:28:56 +0900

    :lipstick: Update: `text` atom components style

--- src/components/MDX/Atoms/Text/index.tsx
+++ src/components/MDX/Atoms/Text/index.tsx
@@ -1,13 +1,13 @@
 const Text = () => {}
 
 const Paragraph = (props: any) => (
-    <p {...props} className="text-base my-2 leading-7" />
+    <p {...props} className=" my-2 text-base font-normal leading-7" />
 )
 const Italic = (props: any) => <em {...props} className="text-base italic" />
 const Link = (props: any) => (
     <a
         {...props}
-        className="text-base font-medium hover:underline hover:text-rose-600 dark:hover:text-rose-400"
+        className="mx-0.5 text-base underline font-medium text-neutral-600 dark:text-neutral-400 hover:text-green-600 dark:hover:text-green-400"
     />
 )
 const Bold = (props: any) => (
