0a10d35121adc6821db3a32fac34df6a1fd9765a
Danpa_cho <danpa725@cau.ac.kr>
2022-09-07 17:26:22 +0900

    :lipstick: Add: `post-banner` component for displaying post banner info

---
+++ src/components/_pages/post/PostBanner.tsx
@@ -0,0 +1,68 @@
+import type { MetaType } from "@typing/post/meta"
+import type { SeriesHeaderProps } from "../../_common/SeriesContainer"
+
+import { Banner, SeriesContainer } from "@components/_common"
+
+import PostMeta from "./PostMeta"
+
+import { config } from "blog.config"
+
+interface PostBannerProps
+    extends MetaType,
+        Partial<Omit<SeriesHeaderProps, "hex">> {}
+function PostBanner({
+    title,
+    color,
+    tags,
+    preview,
+    category,
+    update,
+    author,
+    seriesInfoArray,
+    seriesTitle,
+    seriesCount,
+}: PostBannerProps) {
+    const isSeriescExsists =
+        seriesTitle !== undefined &&
+        seriesInfoArray !== undefined &&
+        seriesCount !== undefined
+
+    return (
+        <div
+            className={`w-full flex flex-col gap-10 pb-6 border-b border-b-neutral-200 dark:border-b-neutral-600`}
+        >
+            <Banner description={preview} title={title} hex={color} />
+
+            <div className="flex flex-col gap-4 items-start justify-center">
+                <div className="flex flex-col gap-2">
+                    <PostMeta
+                        hex={color}
+                        metaArray={[
+                            { content: category, path: `/${category}` },
+                            update,
+                            {
+                                content: author,
+                                path: config.navigationMenu[2].path,
+                            },
+                        ]}
+                    />
+                    <PostMeta
+                        hex={color}
+                        metaArray={tags.map((tag) => `# ${tag}`)}
+                    />
+                </div>
+
+                {isSeriescExsists && (
+                    <SeriesContainer
+                        hex={color}
+                        seriesInfoArray={seriesInfoArray}
+                        seriesTitle={seriesTitle}
+                        seriesCount={seriesCount}
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default PostBanner
