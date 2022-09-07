5ece462dd1c7c048bbec38d33820f7c20619690e
Danpa_cho <danpa725@cau.ac.kr>
2022-09-07 17:25:22 +0900

    :lipstick: Add: `post-toc` component for table of contents of post

---
+++ src/components/_pages/post/PostToc.tsx
@@ -0,0 +1,88 @@
+import type { TableOfContentsType } from "@lib/remark/getTableOfContents"
+
+import { useTocValue } from "@components/TocProvider"
+
+const LinkBtnStyle = {
+    h1: "pb-1 py-1 border-l border-gray-300 dark:border-gray-400 hover:border-black dark:hover:border-transparent",
+    h2: "pl-2 pt-0.5",
+} as const
+type LinkBtnType = keyof typeof LinkBtnStyle
+
+const focusedStyle =
+    "font-bold text-black dark:text-white translate-x-1" as const
+const LinkBtn = ({
+    title,
+    href,
+    type,
+    isFoucused,
+    children,
+}: {
+    title: string
+    href: string
+    isFoucused: boolean
+    type: LinkBtnType
+    children?: React.ReactNode
+}) => {
+    return (
+        <div
+            className={`
+            ${LinkBtnStyle[type]} 
+            ${isFoucused && "border-black"} 
+            pl-2 transition`}
+        >
+            <a className="w-full" href={href}>
+                <p
+                    className={`
+                    ${isFoucused && focusedStyle} 
+                    transition w-40 truncate text-sm 
+                    text-gray-400 hover:text-black
+                    dark:text-gray-400 dark:hover:text-gray-100`}
+                >
+                    {title}
+                </p>
+            </a>
+            {children}
+        </div>
+    )
+}
+
+const TocStyle = {
+    position: "fixed top-0 left-[5%]",
+    layout: "hidden xl:flex flex-col justify-center items-start gap-0",
+    box: "h-full w-fit max-h",
+    text: "hover:text-black dark:hover:text-gray-200",
+} as const
+interface PostTocProps {
+    toc: TableOfContentsType[]
+}
+function PostToc({ toc }: PostTocProps) {
+    const { title } = useTocValue()
+    return (
+        <nav
+            className={`${TocStyle.position} ${TocStyle.layout} ${TocStyle.box} ${TocStyle.text}`}
+        >
+            {toc.map((tocDepth1) => {
+                const { children } = tocDepth1
+                return (
+                    <LinkBtn
+                        type="h1"
+                        isFoucused={title === tocDepth1.title}
+                        key={tocDepth1.title}
+                        {...tocDepth1}
+                    >
+                        {children?.map((tocDepth2) => (
+                            <LinkBtn
+                                type="h2"
+                                isFoucused={title === tocDepth2.title}
+                                key={tocDepth2.title}
+                                {...tocDepth2}
+                            />
+                        ))}
+                    </LinkBtn>
+                )
+            })}
+        </nav>
+    )
+}
+
+export default PostToc
