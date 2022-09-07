9bab4350540b671c635c7f51262f92dcfea7c1ef
Danpa_cho <danpa725@cau.ac.kr>
2022-09-07 17:18:25 +0900

    :lipstick: Update: `nav` component dark theme color

--- src/components/_common/Nav.tsx
+++ src/components/_common/Nav.tsx
@@ -6,14 +6,14 @@ import ThemeBtn from "@components/ThemeBtn"
 const NavStyle = {
     layout: "fixed bottom-[-0.1px] left-0 flex flex-row items-start justify-center gap-2 md:left-[unset] md:gap-0 md:top-0 md:right-0 md:flex-col lg:right-[5%] xl:right-[11%]",
     box: "w-full py-2 md:w-fit",
-    bg: "bg-white dark:bg-neutral-900 md:dark:bg-transparent md:bg-transparent",
-    text: "dark:text-gray-400 dark:hover:text-gray-500 text-gray-400 hover:text-black",
     border: "border-t border-t-gray-300 md:border-t-0",
+    bg: "bg-white dark:bg-neutral-900 md:dark:bg-transparent md:bg-transparent",
+    text: "text-gray-400 hover:text-black dark:hover:text-gray-300 ",
 } as const
 
 const NavBtnStyle = {
     box: "py-1 mx-2 md:px-2 transition",
-    border: "border-b border-b-transparent hover:border-b-black dark:hover:border-b-gray-100 md:border-l md:border-b-0 md:hover:border-l-black",
+    border: "border-b border-b-transparent hover:border-b-black dark:hover:border-b-gray-100 md:border-b-0 md:border-l border-l-gray-300 md:dark:border-l-gray-400 md:hover:border-l-black md:dark:hover:border-l-transparent",
     text: "text-sm font-normal hover:text-black dark:hover:text-gray-100",
 } as const
 
