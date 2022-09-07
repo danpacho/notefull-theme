e7dc9e3423b3bb94de836013e433634a09a20478
Danpa_cho <danpa725@cau.ac.kr>
2022-09-07 17:31:34 +0900

    :lipstick: Update: resize `tw` layout width for large device size

--- src/styles/tailwind.util.ts
+++ src/styles/tailwind.util.ts
@@ -1,5 +1,5 @@
 const tw = {
-    layout: "w-full min-h-screen h-max md:w-3/4 lg:w-4/6 xl:w-3/5",
+    layout: "w-full min-h-screen h-max md:w-3/4 lg:w-4/6 xl:w-3/5 2xl:w-1/2",
     border: "border dark:border-gray-500 hover:border-black dark:hover:border-gray-100",
 } as const
 
