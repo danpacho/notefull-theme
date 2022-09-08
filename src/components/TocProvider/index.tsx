bde43b25c110fc9c960f40cf280b75370e5dedf1
Danpa_cho <danpa725@cau.ac.kr>
2022-09-08 14:52:10 +0900

    :recycle: Refactor: `toc-provider` readability and `value` & `action` name

--- src/components/TocProvider/index.tsx
+++ src/components/TocProvider/index.tsx
@@ -6,43 +6,57 @@ import {
     useCallback,
 } from "react"
 
-const TocValueContext = createContext<string>("")
-const TocSetterContext = createContext<(text: string) => void>(() => {})
+type TocValueType = string
+type TocActionType = (text: string) => void
+const TocValueContext = createContext<TocValueType>("")
+const TocSetterContext = createContext<TocActionType>(() => {})
 
 function TocProvider({ children }: PropsWithChildren<React.ReactNode>) {
-    const [title, setTitle] = useState<string>("")
-    const action = useCallback((text: string) => {
-        setTitle(text)
+    const [activeTitle, setActiveTitle] = useState<TocValueType>("")
+    const cachedAction = useCallback((text: string) => {
+        setActiveTitle(text)
     }, [])
 
     return (
-        <TocSetterContext.Provider value={action}>
-            <TocValueContext.Provider value={title}>
+        <TocSetterContext.Provider value={cachedAction}>
+            <TocValueContext.Provider value={activeTitle}>
                 {children}
             </TocValueContext.Provider>
         </TocSetterContext.Provider>
     )
 }
 
-const useTocValue = () => {
-    const value = useContext(TocValueContext)
-    if (value == undefined)
-        throw Error(
-            "hook is called outside of TocProvider. Value must be access inside of provider"
-        )
-
-    return { title: value }
-}
-
-const useTocAction = () => {
-    const setTitle = useContext(TocSetterContext)
-    if (setTitle === undefined)
-        throw Error(
+const useTocValue = (): {
+    activeTitle: TocValueType
+} => {
+    const activeTitle = useContext(TocValueContext)
+    if (activeTitle === undefined) {
+        console.error(
             "hook is called outside of TocProvider. Action must be access inside of provider"
         )
+        return {
+            activeTitle: "",
+        }
+    }
+
+    return { activeTitle }
+}
+
+const useTocAction = (): {
+    setActiveTitle: TocActionType
+} => {
+    const setActiveTitle = useContext(TocSetterContext)
+    if (setActiveTitle === undefined) {
+        console.error(
+            "hook is called outside of TocProvider. Action must be access inside of provider"
+        )
+        return {
+            setActiveTitle: () => {},
+        }
+    }
 
     return {
-        setTitle,
+        setActiveTitle,
     }
 }
 
