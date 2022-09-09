601b2eb9a20c81b3ae535408655620c91602e11c
Danpa_cho <danpa725@cau.ac.kr>
2022-09-09 19:50:10 +0900

    :sparkles: Update: add `banner` component at `404`/`505` error page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,7 +1,16 @@
-import { PageType } from "@typing/page"
+import type { PageType } from "@typing/page"
+
+import { Banner } from "@components/_common"
+import { config } from "blog.config"
 
 function ErrorPage() {
-    return <div>500</div>
+    return (
+        <Banner
+            title="Oops!"
+            description="sorry, there is no content in this link!"
+            hex={config.themeColor}
+        />
+    )
 }
 
 ErrorPage.displayName = "Error" as PageType
