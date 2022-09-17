0adb45ad5cf747c9adbea24ff7a0d7787f1c7351
Danpa_cho <danpa725@cau.ac.kr>
2022-09-17 14:37:43 +0900

    :lipstick: Update: fix background style bug, chage `bg` to `border`

--- src/components/MDX/Atoms/Divider.tsx
+++ src/components/MDX/Atoms/Divider.tsx
@@ -1,5 +1,7 @@
 function Divider() {
-    return <hr className="w-full h-[1px] bg-neutral-200 dark:bg-neutral-600" />
+    return (
+        <hr className="w-full h-[1px] border-neutral-200 dark:border-neutral-600" />
+    )
 }
 
 export default Divider
