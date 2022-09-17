c035faf886b65959c3cd6fd88edd9159dec40182
Danpa_cho <danpa725@cau.ac.kr>
2022-09-17 14:38:24 +0900

    :lipstick: Update: support nested list style type

--- src/components/MDX/Atoms/List/index.tsx
+++ src/components/MDX/Atoms/List/index.tsx
@@ -1,12 +1,17 @@
 const List = () => {}
 
-const Li = (props: any) => <li {...props} className="" />
-//!TODO: nesting list-style-type
+const Li = (props: any) => (
+    <li
+        {...props}
+        className="[&>ul]:list-circle [&>ul>li>ul]:list-square 
+                    [&>ol]:list-roman [&>ol>li>ol]:list-upper-alpha [&>ol>li>ol>li>ol]:list-roman"
+    />
+)
 const Ol = (props: any) => (
-    <ol {...props} className="ml-5 leading-7 list-decimal" />
+    <ol {...props} className="ml-5 leading-7 md:leading-8 list-decimal" />
 )
 const Ul = (props: any) => (
-    <ul {...props} className="ml-4 leading-7 list-circle-filled" />
+    <ul {...props} className="ml-5 leading-7 md:leading-8 list-circle-filled" />
 )
 
 List.Li = Li
