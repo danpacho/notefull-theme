71577dd83fcf97fa072b51c66829a87cbb37c2b9
Danpa_cho <danpa725@cau.ac.kr>
2023-05-23 10:07:19 +0900

    :recycle: refactor: update blog data interface and description

--- src/interface/page/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @note this is useful, when you have to distinguish page or set different style or layout for each pages in `_app.tsx`.
- */
-export type PageType = "Home" | "Category" | "Post" | "Profile" | "Error"
